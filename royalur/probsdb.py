7dbd310c061f32f137aab14e2a13712b131eabb4
Matthew Sheby <44249925+msheby@users.noreply.github.com>
2021-01-16 21:06:44 -0800

    remove support for compressed files; use struct

--- royalur/probsdb.py
+++ royalur/probsdb.py
@@ -23,103 +23,69 @@ Per-Position win probabilities for the full game space.
 """
 from __future__ import absolute_import
 
+import struct
+
 from .urcore import TOTAL_POSITIONS, board2Index, index2Board
 
 __all__ = ["PositionsWinProbs"]
-
-import os.path
-import gzip
-import bz2
-from contextlib import contextmanager
-
-
-@contextmanager
-def fileFromName(fname):
-    """A Python file object from (possibly compressed) disk file.
-
-    If file has a common suffix (.gz,.bz2) use that as a guide. If fname does
-    not exist, look for a compressed file with the same stem.
-
-    :param fname: file name
-    """
-
-    if os.path.exists(fname):
-        fname = os.path.realpath(fname)
-        if fname.endswith(".gz"):
-            return gzip.open(fname)
-        if fname.endswith(".bz2"):
-            return bz2.BZ2File(fname)
-        return open(fname, "rb")
-    if os.path.exists(fname + ".gz"):
-        return gzip.open(fname + ".gz")
-    if os.path.exists(fname + ".bz2"):
-        return bz2.BZ2File(fname + ".bz2")
-
-    raise IOError("no such file " + fname)
 
 
 class PositionsWinProbs(object):
     """ Win probability for Green (on play) for each ROGOUR position. """
 
-    def __init__(self, fname=None):
-        if fname:
-            self.load(fname)
+    def __init__(self, filename=None):
+        if filename:
+            self.load(filename)
         else:
-            self.b = bytearray(b'\xff') * (4 * TOTAL_POSITIONS)
             self.wsize = 4
+            self.b = bytearray(b'\xff') * self.wsize * TOTAL_POSITIONS
 
-    def load(self, fname):
-        with fileFromName(fname) as f:
+
+    def load(self, filename):
+        with open(filename, "rb") as f:
             self.b = bytearray(f.read())
         if len(self.b) == 4 * TOTAL_POSITIONS:
             self.wsize = 4
         elif len(self.b) == 2 * TOTAL_POSITIONS:
             self.wsize = 2
         else:
-            assert False, "corrupt %s, read only %d" % (fname, len(self.b))
+            raise ValueError("corrupt {0}, read only {1}".format(filename, len(self.b)))
 
-    def save(self, fname):
-        with open(fname, "wb") as f:
+
+    def save(self, filename):
+        with open(filename, "wb") as f:
             f.write(self.b)
 
+
     def board2key(self, board):
-        """Return the db internal 'position'. This happens to be the offset into one humongus
+        """Return the db internal 'position'. This happens to be the offset into one humongous
         byte array.
         """
         return self.wsize * board2Index(board)
+
 
     def key2board(self, key):
         """ Return the key of the board associated with this position. """
         return index2Board(key//self.wsize)
 
+
     def get(self, bpos):
         """ Get the win probability associated with position ``bpos``. """
         if self.wsize == 4:
-            x1, x2, x3, x4 = self.b[bpos:bpos+4]
-            v = (x1 << 24) + (x2 << 16) + (x3 << 8) + x4
-            if v == 0xffffffff:
-                return None
-            return v/2.**31
+            v = struct.unpack_from(">l", self.b, bpos)[0]
+            return v/(2.0**31) if v != -1 else None
         elif self.wsize == 2:
-            x1, x2 = self.b[bpos:bpos+2]
-            v = (x1 << 8) + x2
-            if v == 0xffff:
-                return None
-            return v/(2.**16-1)
-        assert False
+            v = struct.unpack_from(">H", self.b, bpos)[0]
+            return v/(-1 + 2.0**16) if v != 65535 else None
+
 
     def set(self, bpos, pr):
         """ Set the win probability associated with position ``bpos`` to ``pr``. """
-
         if self.wsize == 4:
-            v = int(pr * 2**31)
-            self.b[bpos:bpos+4] = v >> 24, (v >>
-                                            16) & 0xff, (v >> 8) & 0xff, v & 0xff
+            struct.pack_into(">l", self.b, bpos, int(pr * 2.0**31))
         elif self.wsize == 2:
-            v = int(pr * (2**16-1))
-            self.b[bpos:bpos+2] = v >> 8, v & 0xff
-        else:
-            assert False
+            struct.pack_into(">H", self.b, bpos, int(pr * (-1 + 2**16)))
+
 
     # convenience
 
