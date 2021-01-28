db455e6360c1c7e927ac3fbb9a6b5fcb876997e9
Matthew Sheby <44249925+msheby@users.noreply.github.com>
2021-01-27 21:50:16 -0800

    reduce the number of unneeded duplicate calls

--- scripts/makedb.py
+++ scripts/makedb.py
@@ -21,11 +21,11 @@ It is here for educational purposes.
 from __future__ import print_function
 from __future__ import absolute_import
 
-from royalur import *
+from royalur import allMoves, gameOver, positionsIterator, reverseBoard, PositionsWinProbs
 from royalur.humanStrategies import totPips2s
 
 
-def ply1PartsFullRecpt(board, rboard, db):
+def ply1PartsFullRecpt(board, reversed_board, db):
     pWinOnMove = 0
     prReverse = 0
     rest = []
@@ -35,18 +35,18 @@ def ply1PartsFullRecpt(board, rboard, db):
 
     for pr, pips in ((1, 0), (4, 1), (6, 2), (4, 3), (1, 4)):
         am = allMoves(board, pips)
-        if pips == 0 or (len(am) == 1 and not am[0][1] and am[0][0] == rboard):
+        if pips == 0 or (len(am) == 1 and not am[0][1] and am[0][0] == reversed_board):
             prReverse += pr
             if pips > 0:
                 rest.append(None)
             continue
 
-        for b, e in am:
+        for b, _e in am:
             if gameOver(b):
                 assert pWinOnMove == 0
                 pWinOnMove = pr
                 break
-        if pWinOnMove > 0:
+        if pWinOnMove:
             rest.append(None)
             continue
 
@@ -54,14 +54,13 @@ def ply1PartsFullRecpt(board, rboard, db):
         for b, e in am:
             brds[0 if e else 1].append(db.board2key(b))
         rest.append(brds)
-    return [pWinOnMove, prReverse, rest]
+    return (pWinOnMove, prReverse, rest)
 
 
-def ply1BothFullRecpt(board, db):
-    r = reverseBoard(board)
-    A = ply1PartsFullRecpt(board, r, db)
-    B = ply1PartsFullRecpt(r, board, db)
-    return (A, B)
+def ply1BothFullRecpt(board, reversed_board, db):
+    a = ply1PartsFullRecpt(board, reversed_board, db)
+    b = ply1PartsFullRecpt(reversed_board, board, db)
+    return (a, b)
 
 
 def ply1fr(recpt, db):
@@ -71,7 +70,7 @@ def ply1fr(recpt, db):
         if cans:
             g, r = cans
             if not g:
-                m = max([(1-db.get(k)) for k in r])
+                m = max([(1 - db.get(k)) for k in r])
             elif not r:
                 m = max([db.get(k) for k in g])
             else:
@@ -84,106 +83,74 @@ def ply1fr(recpt, db):
 
 def ply1bfr(recpt, db):
     gr, rd = recpt
-    A, p1 = ply1fr(gr, db), gr[1]
-    B, p2 = ply1fr(rd, db), rd[1]
+    a = ply1fr(gr, db)
+    p1 = gr[1]
+    b = ply1fr(rd, db)
+    p2 = rd[1]
 
-    X = (16*A + p1 * (16 - B - p2)) / (256. - p1*p2)
-    Y = (B + p2 * (1 - X)) / 16.
-    if not (0 <= X <= 1 and 0 <= Y <= 1):
-        assert False
-    return X, Y
+    x = (16 * a + p1 * (16 - b - p2)) / (256.0 - p1 * p2)
+    y = (b + p2 * (1 - x)) / 16.0
+    return x, y
 
 
-def halfList(added, db):
-    supdateList = set()
-    updateList = []
-
-    for k in reversed(added):
-        assert k not in supdateList
-        b = db.key2board(k)
-
-        r = db.board2key(reverseBoard(b))
-        if r not in supdateList:
-            updateList.append((k, r))
-            supdateList.add(k)
-
-    return updateList
+def halfList(db, gm, rm):
+    updateSet = set()
+    for board in positionsIterator(gm, rm):
+        key = db.board2key(board)
+        rboard = reverseBoard(board)
+        rkey = db.board2key(rboard)
+        if rkey not in updateSet:
+            updateSet.add(key)
+            yield (totPips2s(board), key, rkey,
+                   ply1BothFullRecpt(board, rboard, db))
 
 
 def main():
     db = PositionsWinProbs()
 
-    filled = 0
-
     for g in range(7):
-        for b in positionsIterator(7, g):
-            db.set(db.board2key(b), 1.0)
-            db.set(db.board2key(reverseBoard(b)), 0.0)
-            filled += 2
+        for board in positionsIterator(7, g):
+            db.set(db.board2key(board), 1)
+            db.set(db.board2key(reverseBoard(board)), 0)
 
     fnbase = "db"
 
     for gm in range(6, -1, -1):
         for rm in range(gm, -1, -1):
             print(gm, rm)
-            added = []
-            for b in positionsIterator(gm, rm):
-                key = db.board2key(b)
-                added.append(key)
-                if db.get(key) is None:
-                    db.set(key, 0.5)
-                    filled += 1
-
-                rk = db.board2key(reverseBoard(b))
-                if db.get(rk) is None:
-                    db.set(rk, 0.5)
-                    filled += 1
-
-            updateList = halfList(added, db)
-            del added
-            tot = len(updateList)
-            print("{0} position pairs.".format(tot))
 
             # Heuristic: sort positions by total (X+O) pip count. The total pip count is a good
             # indicator on how "deep" the positions are in the game tree. This way positions
             # closer to game end are more likely to update first, speeding up convergence.
             #
-            updateList.sort(key=lambda i2: totPips2s(db.key2board(i2[0])))
-
-            frct = []
-            k = 0
-            for key, rkey in updateList:
-                frct.append(ply1BothFullRecpt(db.key2board(key), db))
-                k += 1
-                if k % (36*1024) == 0:
-                    print("%.0f" % (k*100./tot), end=" ")
+            updateList = sorted(halfList(db, gm, rm), key=lambda i2: i2[0])
+            total = len(updateList)
+            tenth = total // 10
+            print("{0} position pairs.".format(total))
             print()
 
-            round = 1
-            maximum_error = 1
-            while maximum_error > 1e-6:
-                print("round {0} ({1} {2})".format(round, gm, rm))
-                dif = 0.0
-                maximum_error = -1
-                count = 0
-                for key, rkey in updateList:
-                    p1, p2 = ply1bfr(frct[count], db)
-
-                    error1 = abs(db.get(key) - p1)
-                    error2 = abs(db.get(rkey) - p2)
-                    dif += error1 + error2
-                    if max(error1, error2) > maximum_error:
-                        maximum_error = max(error1, error2)
+            count = 0
+            iteration_round = 1
+            maximum_error = 1.0
+            while maximum_error > 1.0e-12:
+                print("round {0} ({1} {2})".format(iteration_round, gm, rm))
+                maximum_error = 0.0
+                for _totalPips, key, rkey, data in updateList:
+                    p1, p2 = ply1bfr(data, db)
+                    possible_maximum_error = max(abs(db.get(key) - p1),
+                                                 abs(db.get(rkey) - p2))
+                    if possible_maximum_error > maximum_error:
+                        maximum_error = possible_maximum_error
                     db.set(key,  p1)
                     db.set(rkey, p2)
                     count += 1
-                    if count % (36*1024) == 0:
-                        print(count, int((100.*count)/tot), "%.3g" % maximum_error, ".")
-                round += 1
-                print(maximum_error, dif, dif/(2*tot), tot, filled)
-            del frct
+                    if count % tenth == 0:
+                        print("{0} {1} {2}".format(count, int(100.0 * count / total), maximum_error))
+                count = 0
+                iteration_round += 1
+                print("{0} {1}".format(maximum_error, total))
             del updateList
-            db.save("{0}.inpro.bin".format(fnbase))
+    db.save("{0}.inpro.bin".format(fnbase))
 
 
 if __name__ == "__main__":
